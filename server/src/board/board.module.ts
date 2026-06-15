0113373d66f7f87322e07a0b7d3cb9626ab68ddc
UnderProgramer <ajdjskq13324@gmail.com>
2026-06-15 16:20:31 +0900

    fix: add JwtModule to BoardModule for JwtAuthGuard

--- server/src/board/board.module.ts
+++ server/src/board/board.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { BoardController } from './board.controller';
 import { BoardRepository } from './board.repository';
 import { BoardService } from './board.service';
 
 @Module({
+  imports: [JwtModule.register({})],
   controllers: [BoardController],
   providers: [BoardService, BoardRepository],
 })
